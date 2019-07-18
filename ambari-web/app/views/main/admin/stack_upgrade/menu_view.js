3f704bf2db149020b2d708f1b6631537abc2e559
Alexander Ermakov <ean@arenadata.io>
2019-07-18 22:17:06 +0300

    ADH-949 Enabled version control menu

--- ambari-web/app/views/main/admin/stack_upgrade/menu_view.js
+++ ambari-web/app/views/main/admin/stack_upgrade/menu_view.js
@@ -30,7 +30,7 @@ App.MainAdminStackMenuView = Em.CollectionView.extend({
         label: Em.I18n.t('common.stack'),
         routing: 'services'
       })
-      /*,
+      ,
       Em.Object.create({
         name: 'versions',
         label: Em.I18n.t('common.versions'),
@@ -43,7 +43,7 @@ App.MainAdminStackMenuView = Em.CollectionView.extend({
         label: Em.I18n.t('common.upgrade.history'),
         routing: 'upgradeHistory',
         hidden: !App.get('stackVersionsAvailable')
-      })*/
+      })
     ]
   }.property('App.stackVersionsAvailable'),
 
