fb311973a6f2206accecf95d5618d125fcca2651
Alexander Ermakov <ean@arenadata.io>
2019-09-24 10:29:59 +0300

    ADH-949 Added Current Stack flag for Upgrade Option

--- ambari-server/src/main/resources/upgrade/ddl/Ambari-DDL-Postgres-UPGRADE-2.5.sql
+++ ambari-server/src/main/resources/upgrade/ddl/Ambari-DDL-Postgres-UPGRADE-2.5.sql
@@ -10,5 +10,15 @@ DO $$
                 UPDATE ambari.cluster_version SET state='CURRENT'
                 where repo_version_id=(SELECT repo_version_id FROM ambari.repo_version);
         END IF ;
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'ambari'
+              AND    table_name = 'host_version'
+            )
+        THEN
+                UPDATE ambari.host_version SET state='INSTALLED';
+                UPDATE ambari.host_version SET state='CURRENT' WHERE repo_version_id = (select max(repo_version_id) from ambari.repo_version) and state='INSTALLED';
+        END IF ;
     END
    $$ ;
